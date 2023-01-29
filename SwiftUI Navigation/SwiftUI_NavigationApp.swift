1e03ee21bd0f6eb76e627c787d96d4ac963bd018
Parkhyunwoo <parkhj0423@naver.com>
2023-01-29 20:26:51 +0900

    [parkhj0423] init

---
+++ SwiftUI Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI Navigation
+//
+//  Created by 박현우 on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
