412681b70eb2bb6b00511b928d4d3e0e6b49629e
Parkhyunwoo <parkhj0423@naver.com>
2023-01-29 21:53:15 +0900

    [parkhj0423] Enum으로 변경

--- SwiftUI Navigation/ContentView.swift
+++ SwiftUI Navigation/ContentView.swift
@@ -4,23 +4,32 @@
 //
 //  Created by 박현우 on 2023/01/29.
 //
-
 import SwiftUI
+import SwiftUINavigation
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+
+final class FeatureViewModel : ObservableObject {
+    @Published var destination: Destination?
+
+      enum Destination {
+        case alert(String)
+        case edit(String)
+        case meeting(String)
+        case record(String)
+      }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+struct FeatureView: View {
+    @StateObject var viewModel : FeatureViewModel = FeatureViewModel()
+    
+ var body: some View {
+   Button("Show sheet") {
+       viewModel.destination = .edit(WithState(initialValue: Value, content: <#T##(Binding<Value>) -> View#>))
+//     viewModel.presentedValue = "Hello!"
+   }
+//   .sheet(unwrapping: $viewModel.presentedValue) { $value in
+//     TextField("Value", text: $value)
+//   }
+   
+ }
 }
--- SwiftUI Navigation/SwiftUI_NavigationApp.swift
+++ SwiftUI Navigation/SwiftUI_NavigationApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SwiftUINavigation
 
 @main
 struct SwiftUI_NavigationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FeatureView()
         }
     }
 }
